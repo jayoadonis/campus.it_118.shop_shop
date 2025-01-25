59582022bdff982f09db4d3b880d65105dd69aff
jayoadonis <jayo.adonisraphael@gmail.com>
2025-01-25 15:14:58 +0800

    [chore: 0.0.0] .|. Add simple context stash

---
+++ src/main/php/campus/it_118/shop_shop/utils/Context.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+class Context {
+
+  /**
+   * @var array<string,mixed>
+   */
+  private static array $DATA = [];
+
+  private function __construct() {
+    //REM: Ignore.
+  }
+
+  public static function set(
+    string $key,
+    mixed $data
+  ): void {
+    self::$DATA[$key] = $data;
+  }
+
+  public static function get( string $key ): mixed {
+    return self::$DATA[$key] ?? null;
+  }
+
+  public static function remove( string $key ): mixed {
+    $prevData = self::$DATA[$key];
+
+    unset(self::$DATA[$key]);
+
+    return $prevData;
+  }
+
+  public static function clear(): void {
+    self::$DATA = [];
+  }
+}
